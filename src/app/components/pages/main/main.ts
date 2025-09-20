5032e26f1186b8547fd75b43340bd89490cd7d85
Lucas Machado <machado.lucasalmeida@gmail.com>
2025-09-19 23:04:34 -0300

    Implement code changes to enhance functionality and improve performance

--- src/app/components/pages/main/main.ts
+++ src/app/components/pages/main/main.ts
@@ -20,8 +20,8 @@ export class Main {
     const linkedInUrl = 'https://www.linkedin.com/in/machado-lucasalmeida/';
     window.open(linkedInUrl, '_blank');
   }
-  projectLinks: string[] = ['https://google.com', 'https://youtube.com', 'https://facebook.com', 'https://instagram.com'];
-  repositorytLinks: string[] = ['https://github.com', 'https://github.com', 'https://github.com', 'https://github.com'];
+  projectLinks: string[] = ['https://memory-game-inky-eight.vercel.app/', 'https://youtube.com', 'https://facebook.com', 'https://instagram.com'];
+  repositorytLinks: string[] = ['https://github.com/machadolucasalmeida/memoryGame', 'https://github.com', 'https://github.com', 'https://github.com'];
 
   accessProject(indice: number) {
     const linkProject = this.projectLinks[indice];
