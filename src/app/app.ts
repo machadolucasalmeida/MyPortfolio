590fbba5c5d2c3799c921fc2f3eeb58005fe367c
Lucas Machado <machado.lucasalmeida@gmail.com>
2025-09-19 22:39:55 -0300

    feat: Implement main page layout with hero, about, and projects sections
    - Added main HTML structure for the main page including hero, about, and projects sections.
    - Created SCSS styles for the main page, enhancing the visual design and responsiveness.
    - Implemented component logic for downloading resume and accessing project links.
    - Added unit tests for the main component to ensure functionality.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
-import { Main } from "./pages/main/main";
+import { Main } from "./components/pages/main/main";
 
 @Component({
   selector: 'app-root',
